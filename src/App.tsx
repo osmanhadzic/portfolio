dfd6407020400825c6f0c3e878502be77510ee75
Osman Hadzic <osman.hadzic@secomind.com>
2024-06-12 22:05:55 +0200

    Add AboutMe
    Add component AboutMe
    
    Signed-off-by: Osman Hadzic <osman.hadzic@secomind.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import "./i18n/config";
 import GithubLink from "./components/sections/GithubLink";
 import Header from "./components/sections/Header";
 import Footer from "./components/sections/Footer";
+import AboutMe from "./components/sections/AboutMe";
+import Experience from "./components/sections/Experience";
 
 function App() {
   return (
     <main>
       <GithubLink />
       <Header />
+      <AboutMe />
+      <Experience />
       <Footer />
     </main>
   );
